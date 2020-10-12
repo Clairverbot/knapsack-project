6f5104e263081e700c13d72f5ab6d74f7321c2d5
claier <claire.xw205@gmail.com>
2020-10-12 15:37:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  render() {
+    let allPlayers = [];
+    // todo: make a csv file and iterate through it
+    for (let i = 0; i < 24; i++) {
+      allPlayers.push({
+        name: "Lenny",
+        points:54,
+        price:8000,
+        imgUrl: "./assets/football-player.png",
+      });
+    }
+    return (
+      <div className="App">
+        <div className="calculator-container">
+          <h1>Fantasy Football Calculator</h1>
+          <div>Enter Budget:</div>
+          <div>
+            Icons made by{" "}
+            <a
+              href="https://www.flaticon.com/authors/freepik"
+              title="Freepik"
+              className="link"
+            >
+              Freepik
+            </a>{" "}
+            from{" "}
+            <a
+              href="https://www.flaticon.com/"
+              title="Flaticon"
+              className="link"
+            >
+              {" "}
+              www.flaticon.com
+            </a>
+          </div>
+        </div>
+        <div className="right-panel-container">
+          <div className="football-container"></div>
+          <div className="all-players-container">
+            {allPlayers.map((player, i) => {
+              return (
+                <div index={i} className='player-container'>
+                  <img
+                    src={require(`${player.imgUrl}`)}
+                    alt={player.name}
+                    style={{ width: 96, height: 96 }}
+                  />
+                  <p>{player.name}<br/>
+                  Price: ${player.price}   Points: {player.points}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
